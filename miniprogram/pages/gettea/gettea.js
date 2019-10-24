3b039610fe1ae7bd6c64edff9b71ae8206e22f14
maningning <858698976@qq.com>
2019-10-24 13:08:37 +0800

    first commit

---
+++ miniprogram/pages/gettea/gettea.js
@@ -0,0 +1,83 @@
+// pages/gettea/gettea.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    steps: [
+      {
+        text: '请下单',
+        desc: '喝点儿什么？'
+      },
+      {
+        text: '收到',
+        desc: '正在制作过程中呢'
+      },
+      {
+        text: '马上就好了喔',
+        desc: '再稍稍等等'
+      },
+      {
+        text: '做好啦',
+        desc: '请您取餐'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
